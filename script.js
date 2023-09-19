76a79a6a3076cf4586f24e498a5d56421fc6cc20
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-09-19 13:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll(".btn");
+    const stopButton = document.querySelector(".stop");
+
+    const audioElements = {};
+
+    buttons.forEach((button) => {
+        const soundName = button.id;
+        audioElements[soundName] = new Audio(`sounds/${soundName}.mp3`);
+
+        button.addEventListener("click", () => {
+            stopAllSounds();
+            audioElements[soundName].play();
+        });
+    });
+
+    stopButton.addEventListener("click", () => {
+        stopAllSounds();
+    });
+
+    function stopAllSounds() {
+        buttons.forEach((button) => {
+            const soundName = button.id;
+            audioElements[soundName].pause();
+            audioElements[soundName].currentTime = 0;
+        });
+    }
+});
